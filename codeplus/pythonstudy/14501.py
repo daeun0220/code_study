7cc198f216e69b869a84971088bca2b40a2a3e3e
daeun0220 <daeunkim1284@gmail.com>
2023-02-05 16:44:45 +0900

    top-down dp

--- codeplus/pythonstudy/14501.py
+++ codeplus/pythonstudy/14501.py
@@ -1,4 +1,4 @@
-# 14501 퇴사
+# 14501 퇴사   top-down dp .... 
 n = int(input())
 t_list = []
 p_list = []
@@ -7,23 +7,13 @@ for i in range(n) :
     t_list.append(t)
     p_list.append(p)
 
-dist = [0] * (n+1)
-check = [False] * (n+1)
-'''
-for i in range(n) :
-    if dist[i] == 0 and i+1 + t_list[i] < n+1 and not check[i] :
-        dist[i] += p_list[i]
-        check[i] = True
-'''
-def dp(d, dist, check) :
-    for i in range(n) :
-        if dist[i] == 0 and i+1 + t_list[i] < n+1 and not check[i] :
-            dist[i] += p_list[i]
-            check[i] = True
-            dp(d + t_list[i])
+dp = [0] * (n+1)
+# 역순으로 진행 !!
+for i in range(len(t_list)-2, -1, -1) :
+    if t_list[i] + i < n+1 :
+        dp[i] = max(p_list[i] + dp[i+ t_list[i]], dp[i+1]) # 최댓값 나오게됨
+    else :
+        dp[i] = dp[i+1]
 
-for i in range(n) :
-    dp(i)
-
-print(dp(n))
+print(dp[0])
 
